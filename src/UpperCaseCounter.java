558464ee250a7baa6d494c2eb92fd857c1c43642
BarisOzkucuk <ozkucukbaris27@gmail.com>
2026-02-06 13:26:29 +0300

    Update UpperCaseCounter.java

--- src/UpperCaseCounter.java
+++ src/UpperCaseCounter.java
@@ -1,22 +1,21 @@
 public class UpperCaseCounter {
 
-    // Sınavda istenen METOT kısmı burası:
+    
     public static int countUpperCase(String str) {
-        // 1. Adım: Null kontrolü (Profesyonel kodlama için şart)
+        
         if (str == null) {
             return 0;
         }
 
-        int count = 0; // Büyük harf sayacımız
+        int count = 0; 
 
-        // 2. Adım: String üzerinde döngü (Time Complexity: O(n))
+        /
         for (int i = 0; i < str.length(); i++) {
             
-            // O anki karakteri alıyoruz
+            
             char ch = str.charAt(i);
 
-            // 3. Adım: ASCII mantığı ile kontrol
-            // Karakter 'A' ile 'Z' arasında mı?
+            
             if (ch >= 'A' && ch <= 'Z') {
                 count++;
             }
@@ -25,7 +24,7 @@ public class UpperCaseCounter {
         return count;
     }
 
-    // Test etmek için Main metodu (Bunu GitHub için ekliyoruz)
+   
     public static void main(String[] args) {
         String testInput = "Firat Universitesi Bilgisayar Muhendisligi";
         
@@ -33,6 +32,6 @@ public class UpperCaseCounter {
         int result = countUpperCase(testInput);
         
         System.out.println("Bulunan Büyük Harf Sayısı: " + result);
-        // Beklenen çıktı: F, U, B, M -> Toplam 4 olmalı.
+        
     }
 }
